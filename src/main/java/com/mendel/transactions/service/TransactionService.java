03f51933a1b8972312a558dd6cc0c669a5c1dce9
AlexisK616 <alexiskremis@gmail.com>
2026-06-05 18:10:16 -0300

    feat: implement get ids by type

--- src/main/java/com/mendel/transactions/service/TransactionService.java
+++ src/main/java/com/mendel/transactions/service/TransactionService.java
@@ -2,6 +2,9 @@ package com.mendel.transactions.service;
 
 import com.mendel.transactions.model.Transaction;
 
+import java.util.List;
+
 public interface TransactionService {
     void save(Long id, Transaction transaction);
+    List<Long> getIdsByType(String type);
 }
--- src/main/java/com/mendel/transactions/service/TransactionServiceImpl.java
+++ src/main/java/com/mendel/transactions/service/TransactionServiceImpl.java
@@ -4,6 +4,9 @@ import com.mendel.transactions.model.Transaction;
 import com.mendel.transactions.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class TransactionServiceImpl implements TransactionService{
     private final TransactionRepository repository;
@@ -18,5 +21,12 @@ public class TransactionServiceImpl implements TransactionService{
             throw new IllegalArgumentException("parent transaction not found");
         }
         repository.save(id, transaction);
+    }
+
+    @Override
+    public List<Long> getIdsByType(String type) {
+        return repository.findByType(type).stream()
+                .map(Transaction::getId)
+                .collect(Collectors.toList());
     }
 }
